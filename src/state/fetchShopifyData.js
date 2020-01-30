eb0311d469b2778a51e83ac6e681eacc4bd90298
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-01-30 12:34:11 -0800

    add value and key for Metafields in productsQuery

--- src/state/fetchShopifyData.js
+++ src/state/fetchShopifyData.js
@@ -42,13 +42,10 @@ const productsQuery = client.graphQLClient.query((root) => {
     product.add('descriptionHtml');
     product.add('handle');
     product.add('availableForSale');
-    product.add(
-      "metafield",
-      { args: { key: "about", namespace: "about" } },
-      metafield => {
-        metafield.add("value");
-      }
-    );
+    product.addConnection("metafields", {args: {first: 2}}, metafield => {
+      metafield.add("key")
+      metafield.add("value");
+    });
     product.addConnection("images", {args: {first: 2}}, image => {
       image.add("id");
       image.add("src");

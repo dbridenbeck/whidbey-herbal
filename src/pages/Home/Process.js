9bf9a1312108662d4acea7fa56d23692fcea28ed
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-04 17:02:55 -0800

    use flexbox to align ProcessBlocks

--- src/pages/Home/Process.js
+++ src/pages/Home/Process.js
@@ -1,66 +1,62 @@
 import React from 'react';
-import Wrapper from "../../SharedComponents/ComponentWrapper";
-import StyledH1 from "../../SharedComponents/StyledH1";
+import styled from "styled-components";
+import ComponentWrapper from "../../SharedComponents/ComponentWrapper";
+import StyledH2 from "../../SharedComponents/StyledH2";
 import ProcessBlock from './ProcessBlock';
-
 import harvestLeft from './images/harvestLeft.jpg';
-import harvestRight from "./images/harvestRight.jpg";
 import prepareLeft from "./images/prepareLeft.jpg";
-import prepareRight from "./images/prepareRight.jpg";
-import distill from "./images/distill.jpg";
 import aging from "./images/aging.jpg";
-import bottleLeft from "./images/bottleLeft.jpg";
-import bottleRight from "./images/bottleRight.jpg";
+
+  const ProcessWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: flex-start;
+    width: 100%;
+    border: 1px solid red;
+  `;
 
 const processes = [
   {
-    processTitle: "HARVEST",
+    processTitle: "Harvest",
     description:
       "Our harvest methods vary for each oil. We forage for downed fir bows and use the lavendar from our farm.",
-    imgFlanked: true,
-    imgLeft: `${harvestLeft}`, 
-    imgRight: `${harvestRight}`, 
+    img: `${harvestLeft}`, 
     width: "450",
     height: "450",
     alt: "A watercolor of a fir bow"
   },
   {
-    processTitle: "PREPARE",
+    processTitle: "Prepare",
     description:
     "Plants are trimmed as needed and packed into the still for distillation.",
-    imgFlanked: true, 
-    imgLeft: `${prepareLeft}`, 
-    imgRight: `${prepareRight}`,
+    img: `${prepareLeft}`, 
     width: "200",
     height: "225",
     alt: "A watercolor of a hand pruning a fir bow"
   },
   {
-    processTitle: "DISTILL",
+    processTitle: "Distill",
     description:
     "The still boils water and pushes steam through the plant material to remove the aromatic oils. The steam is then recondensed into essential oil which is tapped off for aging.",
-    imgFlanked: false, 
-    fullWidthImg: `${distill}`,
     width: "1550",
     height: "549",
     alt: "A watercolor of a copper still for distilling essential oils"
   },
   {
-    processTitle: "AGING",
+    processTitle: "Aging",
     description:
     "Oils are 'dried' using epsom salt, and aged between 6 and 12 weeks depending on the plant.",
-    imgFlanked: false, 
-    fullWidthImg: `${aging}`,
+    img: `${aging}`,
     width: "1950",
     height: "700",
     alt: "A watercolor of differnt moon phases"
   },
   {
-    processTitle: "BOTTLE",
+    processTitle: "Bottle",
     description: "We then hand bottle the oils and bring them to market!",
-    imgFlanked: true, 
-    imgLeft: `${bottleLeft}`, 
-    imgRight: `${bottleRight}`,
+    img: `${aging}`,
     width: "200",
     height: "225",
     alt: "A watercolor of a flower"
@@ -69,15 +65,17 @@ const processes = [
 
 const Process = () => {
   return (
-    <Wrapper id="process" maxWidth={"800px"} positionRelative={true}>
-      <StyledH1 colorIsGrey={true} centered={true}>
-        {" "}
-        Our Process{" "}
-      </StyledH1>
-      {processes.map(process => (
-        <ProcessBlock process={process} key={process.processTitle} />
-      ))}
-    </Wrapper>
+    <ComponentWrapper id="process">
+      <StyledH2>
+        
+        Our Process
+      </StyledH2>
+      <ProcessWrapper>
+        {processes.map(process => (
+          <ProcessBlock process={process} key={process.processTitle} />
+        ))}
+      </ProcessWrapper>
+    </ComponentWrapper>
   );
 }
 
--- src/pages/Home/ProcessBlock.js
+++ src/pages/Home/ProcessBlock.js
@@ -3,81 +3,57 @@ import styled from "styled-components";
 import PropTypes from 'prop-types';
 
   const ProcessContainer = styled.div`
-    display: table;
+    display: block;
     position: relative;
-    width: 100%;
-    /* Ternary deals with if process is flanked by two images or not */
-    height: ${props => props.imgFlanked ? "120px" : "190px" };
-    margin-bottom: 200px;
+    width: 40%;
+    border: 1px solid green;
+    :nth-child(even) {
+      margin-top: 230px;
+      align-self: baseline;
+    }
+    :last-child {
+      margin: 136px auto 0 5%;
+    }
   `;
   
   const Info = styled.div`
-    position: absolute;
-    top: ${props => (props.imgFlanked ? "10%" : "45%")};
-    /* the "Distill" process needs some special casing due to it's image dimensions */
-    left: ${props => (props.processTitle === "DISTILL" ? "20%" : "29%")};
-    width: ${props => (props.processTitle === "DISTILL" ? "60%" : "42%")};
-    margin-top: ${process.imgFlanked ? "2.25%" : "0"};
-    z-index: 2;
+    position: relative;
     .title {
-      margin: 5% 0 5px 0;
+      font-family: "Domine", sans-serif;
       font-weight: normal;
-      font-size: 1.5em;
+      font-size: 2.5rem;
       text-align: center;
       color: #787878;
     }
     .description {
       margin: 0 auto;
       padding: 0;
-      font-size: 0.875em;
-      line-height: 1.5em;
+      font-size: 1.5rem;
+      line-height: 1.375em;
+      font-weight: 300;
       text-align: center;
       font-style: normal;
       letter-spacing: 0.01em;
       color: #787878;
     }
   `;
+    
+const Img = styled.img`
+  display: block;
+  float: left;
+  width: 30%;
+  height: auto;
+`;
   
-  const FlankImgWrapper = styled.div`
-    display: table;
-    position: relative;
-    width: 100%;
-    height: 100%;
-  `;
-  
-  const ImgLeft = styled.img`
-    display: block;
-    float: left;
-    width: 30%;
-    height: auto;
-  `;
-  
-  const ImgRight = styled.img`
-    display: block;
-    float: right;
-    width: 30%;
-    height: auto;
-  `;
-  
-  const FullWidthImg = styled.img`
-    display: block;
-    width: 100%;
-    height: auto;
-    margin: 0 auto;
-    z-index: 1;
-  `;
-
 const ProcessBlock = ({
   process: {
     imgFlanked,
     processTitle,
     description,
-    imgLeft,
-    imgRight,
+    img,
     width,
     height,
     alt,
-    fullWidthImg
   }
 }) => {
 return (
@@ -89,20 +65,12 @@ return (
       <h3 className="title">{processTitle}</h3>
       <p className="description">{description}</p>
     </Info>
-    {/* Render FlankImgWrapper if process is flanked by images, otherwise use FullWidthImg */}
-    {imgFlanked ? (
-      <FlankImgWrapper>
-        <ImgLeft
-          src={`${imgLeft}`}
+        <Img
+          src={`${img}`}
           width={`${width}`}
           height={`${height}`}
           alt={`${alt}`}
         />
-        <ImgRight src={`${imgRight}`} />
-      </FlankImgWrapper>
-    ) : (
-      <FullWidthImg src={`${fullWidthImg}`} />
-    )}
   </ProcessContainer>
 );
 }

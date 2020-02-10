1caddcea10b7b68898c3634496003de526c3743e
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-10 13:46:41 -0800

    add svg wave to WelcomeContainer

--- src/pages/Home/WelcomeSection.js
+++ src/pages/Home/WelcomeSection.js
@@ -27,6 +27,19 @@ const WelcomeWrapper = styled.div`
     font-weight: normal;
     color: #787878;
   }
+  svg {
+    /* make sure svg wave covers full width of screen */
+    width: 120vw;
+    margin-left: -10%;
+    @media ${device.tablet} {
+      width: 120vw;
+      margin-left: -30%;
+    }
+    @media ${device.laptop} {
+      width: 120vw;
+      margin-left: -50%;
+    }
+  }
 `;
 
 const FarmAndLogoContainer = styled.div`
@@ -63,9 +76,19 @@ const WelcomeSection = () => {
         <FarmIllustration src={`${farm}`} />
       </FarmAndLogoContainer>
       <h1>Whidbey Herbal</h1>
-      <p> Small-batch, handcrafted, seed to bottle. <br />
+      <p>
+        {" "}
+        Small-batch, handcrafted, seed to bottle. <br />
         Grown from the Heart of Whidbey Island.
       </p>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#0099ff"
+          fill-opacity="1"
+          d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,122.7C672,117,768,139,864,170.7C960,203,1056,245,1152,245.3C1248,245,1344,203,1392,181.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
     </WelcomeWrapper>
   );
 };

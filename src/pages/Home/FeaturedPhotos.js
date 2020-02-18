947a5462bb4efe11bc7915d7d4b02ed69601b2a9
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-18 14:41:58 -0800

    make FullWidthSVG responsive, break FeaturedPhotos into own component

---
+++ src/pages/Home/FeaturedPhotos.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from "styled-components";
+import { device } from "../../utils/devices";
+import ComponentWrapper from "../../SharedComponents/ComponentWrapper";
+import Dock from "./images/dock.jpg";
+import HoneyAndBottles from "./images/honey-and-bottles.jpg";
+import WaveSVG from './images/photosectionsvg.svg';
+
+const FullWidthSVG = styled.div`
+  display: block;
+  position: absolute;
+  /* width, height and margin-left keep SVG positioned */
+  width: 200vw;
+  height: 55%;
+  margin-left: -6vw;
+  background-image: url(${WaveSVG});
+  background-position: left top;
+  /* adjust positioning and size of SVG for bigger screens */
+  @media ${device.tablet} {
+    height: 65%;
+    margin-left: -5vw;
+  }
+  @media ${device.largeScreen} {
+    height: 70%;
+    margin-left: -20vw;
+  }
+`;
+  
+const PhotoContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  /* width, left, right, and margins keep PhotoContainer full-width */
+  width: 105vw;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+  img {
+    width: 50%;
+    /* push images closer to p */
+    margin-top: -5%;
+  }
+  .circle-photo {
+    align-self: flex-end;
+    width: 50%;
+    border-radius: 50%;
+    padding: 0 3.2%;
+  }
+  p {
+    /* width 100% ensures photos stay below p */
+    width: 100%;
+    /* padding right keeps p over */
+    padding-right: 45%;
+    /* 6.4% left and right margins ensures this is centered above circle-photo */
+    margin: 10% 6.4% 0 6.4%;
+    font-size: .725rem;
+    font-weight: 300;
+    @media ${device.tablet} {
+      font-size: 1.375rem;
+    }
+  }
+`;
+
+const FeaturedPhotos = () => {
+  return (
+    <ComponentWrapper>
+      <FullWidthSVG />
+      <PhotoContainer>
+        <p>
+          We’re a family farm that grows and distills each of our essential
+          oils, fresh from the heart of beautiful Whidbey Island in the Salish
+          Sea.
+        </p> 
+        <img class="circle-photo" src={HoneyAndBottles} />
+        <img class="right-rect-photo" src={Dock} />
+      </PhotoContainer>
+    </ComponentWrapper>
+  );
+};
+
+export default FeaturedPhotos;

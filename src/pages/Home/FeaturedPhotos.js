8239c05b9cb5ce50b426b75dc52a30cb333709e7
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-18 17:22:20 -0800

    convert <p> into FeaturedText and make absolutely positioned to improve responsiveness for FeaturedPhotos

--- src/pages/Home/FeaturedPhotos.js
+++ src/pages/Home/FeaturedPhotos.js
@@ -11,21 +11,19 @@ const FullWidthSVG = styled.div`
   position: absolute;
   /* width, height and margin-left keep SVG positioned center */
   width: 200vw;
-  height: 55%;
+  height: 75%;
   /* -200px margin offset's componentwrapper's margin */
   margin: -200px 0 0 -6vw;
   background-image: url(${WaveSVG});
   /* adjust positioning and size of SVG for bigger screens */
   @media ${device.tablet} {
-    height: 65%;
     margin-left: -5vw;
   }
   @media ${device.largeScreen} {
-    height: 70%;
     margin-left: -20vw;
   }
 `;
-  
+
 const PhotoContainer = styled.div`
   position: relative;
   display: flex;
@@ -41,37 +39,31 @@ const PhotoContainer = styled.div`
   padding-top: 20%;
   img {
     width: 50%;
-    /* push images closer to p */
-    margin-top: -12.5%;
-    @media ${device.laptop} {
-      margin-top: -8%;
-    }
-    @media ${device.largeScreen} {
-      margin-top: -12%;
-    }
   }
   .circle-photo {
     align-self: flex-end;
-    width: 50%;
     border-radius: 50%;
     padding: 0 3.2%;
   }
-  p {
-    /* width 100% ensures photos stay below p */
-    width: 100%;
-    /* padding right keeps p over */
-    padding-right: 45%;
-    /* 6.4% left and right margins ensures this is centered above circle-photo */
-    margin: 0% 6.4% 0 3.2%;
-    font-size: 0.825rem;
-    line-height: 1.36em;
-    font-weight: 300;
-    @media ${device.tablet} {
-      font-size: 1.5rem;
-    }
-    @media ${device.laptop} {
-      font-size: 2.375rem;
-    }
+`;
+
+const FeaturedText = styled.p`
+  display: block;
+  position: absolute;
+  top: 15%;
+  left: 0;
+  /* width 100% ensures photos stay below p */
+  width: 50%;
+  padding: 0 3.2%;
+  margin: 0;
+  font-size: 0.825rem;
+  line-height: 1.36em;
+  font-weight: 300;
+  @media ${device.tablet} {
+    font-size: 1.5rem;
+  }
+  @media ${device.laptop} {
+    font-size: 2.375rem;
   }
 `;
 
@@ -80,11 +72,11 @@ const FeaturedPhotos = () => {
     <ComponentWrapper>
       <FullWidthSVG />
       <PhotoContainer>
-        <p>
-          We’re a family farm that grows and distills each of our essential
-          oils, fresh from the heart of beautiful Whidbey Island in the Salish
-          Sea.
-        </p> 
+      <FeaturedText>
+        We’re a family farm that grows and distills each of our essential
+        oils, fresh from the heart of beautiful Whidbey Island in the Salish
+        Sea.
+      </FeaturedText> 
         <img class="circle-photo" src={HoneyAndBottles} />
         <img class="right-rect-photo" src={Dock} />
       </PhotoContainer>

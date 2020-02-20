b20205ba1e97562e3c180fb78591b66fb6b2c8a8
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-19 17:56:46 -0800

    create .highlight and position it across breakpoints

--- src/pages/Home/WelcomeSection.js
+++ src/pages/Home/WelcomeSection.js
@@ -11,37 +11,73 @@ const WelcomeWrapper = styled.div`
   @media ${device.largeScreen} {
     max-width: 1000px;
   }
-  h1 {
-    position: relative;
-    width: 100vw;
-    left: 50%;
-    right: 50%;
-    margin: -20% -50vw 0 -46vw;
-    color: black;
-    font-size: 1.75rem;
-    font-weight: bold;
-    @media ${device.tablet} {
-      font-size: 4rem;
-    }
-    @media ${device.laptop} {
-      font-size: 5rem;
-    }
-  }
   img {
     position: relative;
     width: 100%;
     margin: 0 auto;
   }
-`;
+  `;
+
+const Tagline = styled.div`
+  display: block;
+  position: relative;
+  width: 365px;
+  left: 50%;
+  right: 50%;
+  margin: -27% -46vw 0 -46vw;
+  color: black;
+  @media ${device.tablet} {
+    width: 783px;
+  }
+  @media ${device.laptop} {
+    width: 150%;
+  }
+  h1 {
+    display: inline-block;
+    position: relative;
+    font-size: 1.75rem;
+    font-weight: bold;
+    z-index: 5;
+    @media ${device.tablet} {
+      font-size: 3.45rem;
+    }
+    @media ${device.laptop} {
+      font-size: 5rem;
+    }
+  }
+  .highlight {
+    position: absolute;
+    background-color: #E6C564;
+    top: 31%;
+    right: 6%;
+    height: 1.225rem;
+    width: 175px;
+    @media ${device.tablet} {
+      top: 31%;
+      right: 13%;
+      height: 2.5rem;
+      width: 350px;
+    }
+    @media ${device.laptop} {
+      top: 31%;
+      right: 21.5%;
+      height: 3.75rem;
+      width: 510px;
+      }
+    }
+
+  `;
 
 const WelcomeSection = () => {
   return (
     <WelcomeWrapper>
       <img src={`${farm}`} />
-      <h1>
-        {" "}
-        Small batch handcrafted. <br/> Seed-to-bottle.
-      </h1>
+      <Tagline>
+        <h1>
+          Small batch handcrafted. <br/> Seed-to-bottle.
+        </h1>
+        <div className="highlight" />
+      </Tagline>
     </WelcomeWrapper>
   );
 };

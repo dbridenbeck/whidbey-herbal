e496a49de1dae87fc532e281b74f636df7380f36
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-21 12:41:03 -0800

    style AboutText for breakpoints and fit Kayla design

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import styled from "styled-components";
 import farmers from './images/farmers.jpg';
-import Wrapper from "../../SharedComponents/ComponentWrapper";
+import ComponentWrapper from "../../SharedComponents/ComponentWrapper";
 import StyledH2 from "../../SharedComponents/StyledH2";
+import { device } from "../../utils/devices";
 
 const FarmerIllustration = styled.img`
   display: block;
   height: auto;
-  width: 100%;
+  width: 87%;
   max-width: 400px;
   margin: 0 auto;
 `;
 
 const AboutText = styled.p`
   padding: 0 20px;
-  font-size: 0.875em;
+  font-size: 0.875rem;
   line-height: 1.5em;
-  font-style: normal;
-  letter-spacing: 0.01em;
-  color: #787878;
+  font-weight: 300;
+  text-align: center;
+  color: #2e2e2e;
+  @media ${device.tablet} {
+    font-size: 1rem;
+    padding: 0;
+  }
+  @media ${device.laptop} {
+    padding: 0;
+    font-size: 1.5rem;
+  }
 `;
 
 const About = () => {
   return (
-    <Wrapper id="about" maxWidth={"600px"}>
+    <ComponentWrapper id="about" maxWidth={"590px"}>
       <StyledH2 colorIsGrey={true} centered={true}>
         The Farmers
       </StyledH2>
@@ -34,13 +43,13 @@ const About = () => {
         alt="Watercolor image of Sam and Caitlin, the farmers of Whidbey Herbal"
       />
       <AboutText>
-        Caitlin and Sam Stanton are fifth and third generation Northwesterners,
-        respectively. We love our community for its independence, and support of
-        small farms. Our hobbies are backpacking, boats, knitting, and
-        gardening. We live on the farm with our son Forrest and our dogs Bonnie
-        and Merlin.
+        We’re Caitlin and Sam Stanton, and we’re fifth and third generation
+        Northwesterners, respectively. We love our community for its
+        independence, and support of small farms. Our hobbies are backpacking,
+        boats, knitting, and gardening. We live on the farm with our son Forrest
+        and our dogs Bonnie and Merlin.
       </AboutText>
-    </Wrapper>
+    </ComponentWrapper>
   );
 }
 

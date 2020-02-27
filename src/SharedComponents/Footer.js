6e0da207c473f1014db3b97fec98dfff4d58e989
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-02-27 15:07:39 -0800

    make beesAndHoney image webp supported, move WHPattern image to picture component and remove from css background, implement webp support

--- src/SharedComponents/Footer.js
+++ src/SharedComponents/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import styled from 'styled-components';
 import whPattern from './images/wh-pattern.jpg';
+import whPatternWebp from './images/wh-pattern.webp';
 import { device } from "../utils/devices";
 import NewsletterSignup from './NewsletterSignup';
 import ContactAndSocials from './ContactAndSocials';
@@ -15,18 +16,7 @@ import ContactAndSocials from './ContactAndSocials';
     margin: ${props => props.pathname === "/" ? "50px auto 0 0px" : "50px auto 0 -20px"};
     padding: 125px 10px 25px 10px;
     background-color: rgba(230, 197, 100, 1);
-    ::before {
-      content: "";
-      background: url(${whPattern});
-      opacity: 0.04;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-    }
+    overflow-y: hidden;
     @media ${device.tablet} {
       display: flex;
       flex-direction: row;
@@ -34,9 +24,29 @@ import ContactAndSocials from './ContactAndSocials';
     }
   `;
 
+  const WHPatternBackground = styled.picture`
+    img {
+      content: "";
+      background: url(${whPattern});
+      background-size: auto;
+      opacity: 0.04;
+      position: absolute;
+      height: 110%;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+    }
+  `;
+
 const Footer = ({location: {pathname}}) => {
   return (
     <FooterContainer pathname={pathname}>
+      <WHPatternBackground>
+        <source srcSet={`${whPatternWebp}`} type="image/webp" />
+        <source srcSet={`${whPattern}`} type="image/jpeg" />
+        <img src={`${whPattern}`} alt="Cute Woodland Style Line Drawings" />
+      </WHPatternBackground>
       <NewsletterSignup />
       <ContactAndSocials />
     </FooterContainer>

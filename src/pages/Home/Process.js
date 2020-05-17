8ea9e857d779415f6a0309d03c7d20aa8103d289
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-05-17 15:56:18 -0700

    update copy on process and about components on home page

--- src/pages/Home/Process.js
+++ src/pages/Home/Process.js
@@ -39,7 +39,7 @@ const processes = [
   {
     processTitle: "Harvest",
     description:
-      "We choose the most sustainable option for each plant –either wild harvest or farm grown. Our tree oils come from branches knocked down by wind. Healing herbs we grow on our farm.",
+      "We choose the most sustainable option for each plant –either wild harvest or farm grown. Our tree oils come from branches knocked down by wind.",
     img: `${harvest}`,
     webp: `${harvestWebp}`,
     width: "450",
@@ -48,7 +48,8 @@ const processes = [
   },
   {
     processTitle: "Prepare",
-    description: "Plants are hand-trimmed and packed into the still.",
+    description:
+      "Plants are hand-trimmed and packed into the still. We only distill the parts of the plant that yield the truest scent, such as the greenery from trees, or the stem and flower from lavender.",
     img: `${handBright}`,
     webp: `${handBrightWebp}`,
     width: "200",
@@ -68,7 +69,7 @@ const processes = [
   {
     processTitle: "Aging",
     description:
-      "Unlike many mass market oils, we age our oils between 3 to 16 weeks. By aging, the oil can reach its true scent, the exact smell you would get from crushing the fresh plant in your hand.",
+      "Unlike many mass-market oils, we age our oils between 3 to 16 weeks. By aging, the oil can reach its true scent, the exact smell you would get from crushing the fresh plant in your hand.",
     img: `${aging}`,
     webp: `${agingWebp}`,
     width: "1950",
@@ -78,7 +79,7 @@ const processes = [
   {
     processTitle: "Bottle",
     description:
-      "At last, our oils are hand bottled and ready for sale. You can find us at the Bayview Farmers Market, at the lovely stores listed below, or on your doorstep. Online orders are sent in 100% recycled, plastic-free, guilt-free packaging.",
+      "At last, our oils are hand bottled and ready for sale. You can find us at the Bayview Farmers Market, the lovely stores listed below, or on your doorstep. Online orders are sent in 100% recycled, plastic-free, guilt-free packaging.",
     img: `${bottle}`,
     webp: `${bottleWebp}`,
     width: "200",

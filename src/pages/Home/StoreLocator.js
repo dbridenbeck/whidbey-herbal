2a347d5bf3e4be125dbf4fe57876a9486a0326da
Ethan Pierce <ethanpiercepresents@gmail.com>
2022-05-09 19:01:34 -0700

    removing commented code and console log

--- src/pages/Home/StoreLocator.js
+++ src/pages/Home/StoreLocator.js
@@ -4,7 +4,6 @@ import ComponentWrapper from '../../SharedComponents/ComponentWrapper';
 import StyledH2 from '../../SharedComponents/StyledH2';
 import StoreBlock from './StoreBlock';
 import GoogleMapWrapper from './GoogleMapWrapper';
-
 import { device } from '../../utils/devices';
 
 const LocationsWrapper = styled.div`
@@ -55,15 +54,6 @@ const StoreList = styled.div`
 `;
 
 export const stores = [
-  //  {
-  //    storeName: "Worn Path",
-  //    address: `4007 N. Mississippi Ave. \nPortland, OR 97227`,
-  //    lat: 45.552305,
-  //    lng: -122.675666,
-  //    website: "https://www.worn-path.com/",
-  //    googleMapUrl:
-  //      "https://www.google.com/maps/place/Worn+Path/@45.5522862,-122.6778633,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a76b07f44829:0xc963c7d7e7732679!8m2!3d45.5522862!4d-122.6756693"
-  //  },
   {
     storeName: '3 Sisters Market',
     address: '779 Holbrook Road \nCoupeville, WA 98239',
@@ -73,15 +63,6 @@ export const stores = [
     googleMapUrl:
       'https://www.google.com/maps/place/3+Sisters+Market/@48.237249,-122.722152,17z/data=!3m1!4b1!4m5!3m4!1s0x548f8c86bc348f65:0x2ea75e0b77df1645!8m2!3d48.237249!4d-122.719958',
   },
-  // {
-  //   storeName: 'Wish by the Sea',
-  //   address: '208 1st \nSt. Langley, WA 98260',
-  //   lat: 48.040886,
-  //   lng: -122.408415,
-  //   website: 'https://www.facebook.com/wishsisters/',
-  //   googleMapUrl:
-  //     'https://www.google.com/maps/place/Wish+by+the+Sea/@48.0408497,-122.4106087,17z/data=!3m1!4b1!4m5!3m4!1s0x548ff9375096a79f:0xd36c343435ee70eb!8m2!3d48.0408497!4d-122.4084147',
-  // },
   {
     storeName: 'Flying Bear Florist',
     address: '207 1st \nSt. Langley, WA 98260',
@@ -103,7 +84,6 @@ export const stores = [
 ];
 
 const StoreLocator = () => {
-  console.log('CONSOLE', window.innerWidth);
   return (
     <ComponentWrapper id='findstore' backgroundColor={'#FFF3D1'}>
       <StyledH2>Store Locator</StyledH2>

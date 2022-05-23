aaab7286e404c7c5b1ff9ec6bcc3f535bef7cae1
Ethan Pierce <ethanpiercepresents@gmail.com>
2022-05-22 18:47:53 -0700

    updating Ethan Pierce to ContactAndSocials.js

--- src/SharedComponents/ContactAndSocials.js
+++ src/SharedComponents/ContactAndSocials.js
@@ -1,83 +1,81 @@
 import React from 'react';
 import styled from 'styled-components';
-import { device } from "../utils/devices";
+import { device } from '../utils/devices';
 import instagram from './images/instagram.png';
 import facebook from './images/facebook.png';
 
-  const ContactAndSocialsWrapper = styled.div`
+const ContactAndSocialsWrapper = styled.div`
+  display: block;
+  width: 100%;
+  margin-top: 20px;
+  z-index: 5;
+  p {
     display: block;
-    width: 100%;
-    margin-top: 20px;
-    z-index: 5;
-    p {
-      display: block;
-      font-size: 1em;
-      font-style: normal;
-      font-weight: 300;
-      color: #2e2e2e;
-      margin: 20px 0;
+    font-size: 1em;
+    font-style: normal;
+    font-weight: 300;
+    color: #2e2e2e;
+    margin: 20px 0;
+  }
+  .info-text {
+    font-size: 0.825em;
+  }
+  a {
+    font-weight: 300;
+    color: #2e2e2e;
+    text-decoration: none;
+    :hover {
+      color: black;
     }
-    .info-text {
-      font-size: 0.825em;
-    }
-    a {
-      font-weight: 300;
-      color: #2e2e2e;
-      text-decoration: none;
-      :hover {
-        color: black;
-      }
-    }
-    @media ${device.tablet} {
-      width: 35%;
-      max-width: 448px;
-    }
-  `;
+  }
+  @media ${device.tablet} {
+    width: 35%;
+    max-width: 448px;
+  }
+`;
 
-  const SocialIconContainer = styled.div`
-    margin: 0 auto;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    width: 100%;
-  `;
+const SocialIconContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 100%;
+`;
 
-  const SocialIcon = styled.img`
-    display: inline-block;
-    height: 25px;
-    width: 25px;
-    margin: 10px 20px 0 0;
-  `;
-
+const SocialIcon = styled.img`
+  display: inline-block;
+  height: 25px;
+  width: 25px;
+  margin: 10px 20px 0 0;
+`;
 
 const ContactAndSocials = () => {
   return (
     <ContactAndSocialsWrapper>
-      <a href="mailto:hello@whidbeyherbal.com">hello@whidbeyherbal.com</a>
+      <a href='mailto:hello@whidbeyherbal.com'>hello@whidbeyherbal.com</a>
       <p>(360) 386-5831</p>
-      <div  className="info-text">
+      <div className='info-text'>
+        <p>Proud member of the Whidbey Island Grown cooperative.</p>
         <p>
-          Proud member of the Whidbey Island Grown cooperative.
-        </p>
-        <p>
-          Website built by <a href="htt://darren.fun">Darren Bridenbeck</a>
+          Website built by <a href='htt://darren.fun'>Darren Bridenbeck</a> and{' '}
+          <a href='https://github.com/jumpybuns'>Ethan Pierce</a>
         </p>
       </div>
       <SocialIconContainer>
-        <a href="https://www.instagram.com/whidbeyherbal/">
+        <a href='https://www.instagram.com/whidbeyherbal/'>
           <SocialIcon
             src={`${instagram}`}
-            height="512"
-            width="512"
-            alt="Instagram logo"
+            height='512'
+            width='512'
+            alt='Instagram logo'
           />
         </a>
-        <a href="https://www.facebook.com/WhidbeyHerbal/">
+        <a href='https://www.facebook.com/WhidbeyHerbal/'>
           <SocialIcon
             src={`${facebook}`}
-            height="512"
-            width="512"
-            alt="Facebook logo"
+            height='512'
+            width='512'
+            alt='Facebook logo'
           />
         </a>
       </SocialIconContainer>

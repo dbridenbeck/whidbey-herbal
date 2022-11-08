986750527172dd6fb973ae880ef9cdb25fb1d030
darren <darren.bridenbeck@gmail.com>
2022-11-07 20:08:33 -0800

    ugh try and fix dependencies

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,17 +1,20 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloLink } from 'apollo-boost';
+
 import 'cross-fetch/polyfill';
 // config Apollo
 const httpLink = createHttpLink({
   uri: 'https://whidbey-herbal.myshopify.com/api/2020-07/graphql.json',
 });
 
-const middlewareLink = setContext(() => ({
-  headers: {
-    'X-Shopify-Storefront-Access-Token': '837432f0b8059e443da74da036f73f70',
-  },
-}));
+const middlewareLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      'X-Shopify-Storefront-Access-Token': '837432f0b8059e443da74da036f73f70',
+    },
+  });
+  return forward(operation);
+});
 
 const apolloClient = new ApolloClient({
   link: middlewareLink.concat(httpLink),

5091183477d112ee00ce91bb2686ae132ab87f15
darren <darren.bridenbeck@gmail.com>
2022-12-10 12:59:31 -0800

    handle styles on server in _document.tsx

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {
-      // Enabled by default in development, disabled in production to reduce file size,
-      // setting this will override the default for all environments.
-      displayName: true,
-      ssr: true,
-    },
+    styledComponents: true,
   },
   images: {
     domains: ['cdn.shopify.com'],
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,42 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-        <link
-          rel="preload"
-          href="https://fonts.googleapis.com/css?family=Domine:400,700|Open+Sans:300,400,600,700&display=swap"
-          as="style"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Domine:400,700|Open+Sans:300,400,600,700&display=swap"
-        />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-      </Head>
-      <body>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }

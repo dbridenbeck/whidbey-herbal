3b6321bd2faf06d78985463609c5db1272e7908d
darren <darren.bridenbeck@gmail.com>
2023-03-04 11:44:01 -0800

    re-add fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,21 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css?family=Domine:400,700|Open+Sans:300,400,600,700&display=swap"
+            as="style"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Domine:400,700|Open+Sans:300,400,600,700&display=swap"
+          />
+          <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+          <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        </Head>
         <body>
           <Main />
           <NextScript />
